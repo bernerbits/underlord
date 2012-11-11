6e330ad847fb6173ee86bb2b60f5aec4ce1c2ac5
Derek Berner <bernerbits@gmail.com>
2012-11-11 16:19:41 -0600

    nothing new really

--- src/main/java/com/bernerbits/ul/mesh/DungeonGenerator.java
+++ src/main/java/com/bernerbits/ul/mesh/DungeonGenerator.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
-import org.poly2tri.Poly2Tri;
-import org.poly2tri.geometry.polygon.Polygon;
-import org.poly2tri.triangulation.TriangulationPoint;
-import org.poly2tri.triangulation.delaunay.DelaunayTriangle;
 
 import com.bernerbits.ul.texture.Texturizer;
 import com.bernerbits.util.geom.Polygon2D;
@@ -16,8 +12,6 @@ import com.bernerbits.util.geom.SharedVertex2D;
 import com.bernerbits.util.geom.SharedVertexTriangle2D;
 import com.bernerbits.util.geom.SharedVertexTriangleSet2D;
 import com.bernerbits.util.geom.Tesselator;
-import com.jme3.asset.AssetManager;
-import com.jme3.material.Material;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue.ShadowMode;
@@ -26,8 +20,6 @@ import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.VertexBuffer;
-import com.jme3.texture.Texture;
-import com.jme3.texture.Texture.WrapMode;
 import com.jme3.util.BufferUtils;
 
 public class DungeonGenerator {
@@ -68,7 +60,6 @@ public class DungeonGenerator {
 		
 		Mesh floor = new Mesh();
 		floor.setMode(Mesh.Mode.Triangles);
-		//floor.setMode(Mesh.Mode.Lines);
 		
 		List<Vector3f> vertices = new ArrayList<Vector3f>();
 		List<Vector2f> texcoords = new ArrayList<Vector2f>();
@@ -81,19 +72,9 @@ public class DungeonGenerator {
 			normals.add(new Vector3f(0,1,0));
 		}
 		for(SharedVertexTriangle2D tri : triangles.getTriangles()) {
-			//SharedVertex2D last = null;
-			//SharedVertex2D first = null;
 			for(SharedVertex2D v : tri.getCWVertices()) {
-				//if(last != null) {
-				//	indices.add(last.getIndex());
 					indices.add(v.getIndex());
-				//} else {
-				//	first = v;
-				//}
-				//last = v;
 			}
-			//indices.add(last.getIndex());
-			//indices.add(first.getIndex());
 		}
 		
 		floor.setBuffer(VertexBuffer.Type.Position,3,BufferUtils.createFloatBuffer(vertices.toArray(new Vector3f[0])));
@@ -116,7 +97,6 @@ public class DungeonGenerator {
 		List<Integer> indices = new ArrayList<Integer>();
 		
 		wall.setMode(Mesh.Mode.TriangleStrip);
-		//wall.setMode(Mesh.Mode.LineStrip);
 		int index = 0;
 		for(Polygon2DNode node : wallPoly.getNodes()) {
 			indices.add(index);
